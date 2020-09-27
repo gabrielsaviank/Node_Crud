e400d0d68c8bc652ec69367d246ac28c8e819be7
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2020-09-27 16:37:20 -0300

    Yargs parsing

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const chalk = require('chalk');
+// Yargs do the parsing arguments
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2]
 
-if (command === 'add') {
-    console.log('Adding Note')
-} else if (command === 'remove'){
-    ('Removing Note!')
-}
+// Create Add Command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note')
+    }
+})
+
+// Create Remove Command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing Note')
+    }
+})
+
+// Create list Command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('Listing out all notes')
+    }
+})
+
+// Create Read Command
+yargs.command({
+    command: 'read',
+    describe: 'Read a Note',
+    handler: function() {
+        console.log('Reading a Note')
+    }
+})
+
+// add, remove, read, list 
+
+console.log(yargs.argv);
